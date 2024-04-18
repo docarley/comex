007a548e71194f94e5aa955003281e716b40e5a0
Arley <docente.arley@gmail.com>
2024-04-18 17:41:43 -0300

    Criando as classes cliente e endereço

---
+++ src/main/java/br/com/alura/comex/models/Cliente.java
@@ -0,0 +1,46 @@
+package br.com.alura.comex.models;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String email;
+    private String profissao;
+    private String telefone;
+    private Endereco endereco;
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", email='" + email + '\'' +
+                ", profissao='" + profissao + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", endereco=" + endereco.toString() +
+                '}';
+    }
+}
